b9ed2cec52669bc817028ed565fc71c937563447
malcolm staso <mstaso1@gmail.com>
2020-05-05 15:50:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -46,11 +46,13 @@
 
 
 def introduction (name)
-  puts "hello #{name}!"
+  puts "Hi, my name is #{name}."
 end  
 
 
-
+def introduction_with_language (name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end  
 
 
 
